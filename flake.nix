9336837f009cbbee2facf34ebdbf3c870a6e497c
Michael Schneider <michael.schneider@cyberus-technology.de>
2022-09-08 17:00:19 +0200

    change hash

--- flake.nix
+++ flake.nix
@@ -1,14 +1,13 @@
 {
-  description = "Minimal reproducible example for clippy and rustfmt nix pre commit hooks";
+  description = "Minimal reproducible example for clippy and rustfmt nix pre commit hooks.";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/master";
-    naersk.url = "github:nix-community/naersk";
     utils.url = "github:numtide/flake-utils";
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
   };
 
-  outputs = { self, nixpkgs, utils, naersk, pre-commit-hooks }:
+  outputs = { self, nixpkgs, utils, pre-commit-hooks }:
     utils.lib.eachSystem [ utils.lib.system.x86_64-linux ] (system:
       let
         rustfmtHook = pre-commit-hooks.lib.${system}.run {
@@ -26,4 +25,3 @@
       }
   );
 }
-
